ebee373ad9b2fd83c1cf49cc0fac0c3953d5a98f
OlgaTar <o_v_v_a@mail.ru>
2022-11-22 16:08:17 +0300

    First edition

--- src/ru/progwards/java1/lessons/custing/Figures.java
+++ src/ru/progwards/java1/lessons/custing/Figures.java
@@ -16,23 +16,23 @@ public class Figures {
         return S_Tr;
     }
     public static double squareVsTraiange(double p){
-     double S_vs_T = square(2.3)/triangle(4.2);
+     double S_vs_T = square(p)/triangle(p);
      return S_vs_T;
     }
     public static double squareVsCircle(double p){
-        double S_vs_C = square(6.0)/circle(4.1);
+        double S_vs_C = square(p)/circle(p);
         return S_vs_C;
     }
     public static double triangleVsCircle(double p){
-        double Tr_vs_C = triangle(1.1)/circle(6.3);
+        double Tr_vs_C = triangle(p)/circle(p);
         return Tr_vs_C;
     }
     /*public static void main(String[] args) {
         System.out.println(circle(5.0));
         System.out.println(square(4.8));
         System.out.println(triangle(8.2));
-        System.out.println(squareVsTraiange(2.0));
-        System.out.println(squareVsCircle(2.0));
-        System.out.println(triangleVsCircle(2.0));
+        System.out.println(squareVsTraiange(56.98796587545567));
+        System.out.println(squareVsCircle(56.98796587545567));
+        System.out.println(triangleVsCircle(56.98796587545567));
     }*/
 }
