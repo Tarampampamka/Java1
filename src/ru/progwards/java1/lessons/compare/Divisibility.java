a58ae87b96261a23e693fd4f9852264477bc7a39
OlgaTar <o_v_v_a@mail.ru>
2022-11-25 19:21:03 +0300

    First edition

--- src/ru/progwards/java1/lessons/compare/Divisibility.java
+++ src/ru/progwards/java1/lessons/compare/Divisibility.java
@@ -15,7 +15,7 @@ public class Divisibility {
         }
     }
 
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         check(13,0);
-    }
+    }*/
 }
