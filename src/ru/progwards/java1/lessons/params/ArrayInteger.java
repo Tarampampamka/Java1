b8cfa59ad9578663f70e3325c61ac1da859f2bb5
OlgaTar <o_v_v_a@mail.ru>
2023-02-28 17:53:13 +0300

    First edition

--- src/ru/progwards/java1/lessons/params/ArrayInteger.java
+++ src/ru/progwards/java1/lessons/params/ArrayInteger.java
@@ -1,58 +1,68 @@
 package ru.progwards.java1.lessons.params;
-
-import java.nio.ByteBuffer;
 import java.util.Arrays;
-
 public class ArrayInteger {
     byte[] digits;
     int n;
+    //инициализирует класс, с максимальной точностью n цифр (размер массива)
     ArrayInteger(int n){
         digits=new byte[n];
         this.n=n;
     }
     //установить свое значение, взяв его из value (уложить числовое значение value во внутренний массив)
-    void fromString(String value) { 
+    void fromString(String value) {
         String[] strArr = value.split("");
         digits = new byte[strArr.length];
         for (int i = 0; i < strArr.length; i++) {
             digits[i] = Byte.parseByte(strArr[i]);
         }
+        System.out.println(Arrays.toString(digits));
     }
     @Override
     public String toString(){
         return Arrays.toString(digits);
     }
 
-    boolean add(ArrayInteger num){
-        boolean ok=true;
-        byte[] finMass=new byte[n];
-        int lengthNewMas;
-        if (num.n > digits.length) {
-            lengthNewMas = num.n;
-        } else {
-            lengthNewMas = digits.length;
-        }
-        ArrayInteger fin = new ArrayInteger(lengthNewMas);
-        int firstD= ByteBuffer.wrap(digits).getInt();
-        int secondD= ByteBuffer.wrap(num.digits).getInt();
-        int sum = firstD+secondD;
-        String sumStr = Integer.toString(sum);
+    //сложить 2 числа, используя массив digits, фактически столбиком, как учили в школе в 5-м (или каком там)
+    // классе. Результат поместить в свой экземпляр ArrayInteger, у которого был вызван метод.
+    // При переполнении вернуть false, при этом само число сбросить в 0
+    boolean add(ArrayInteger num) {
+        boolean ok = true;
+        //int j = n - 1;
+        String out1 = "", out2 = "", outSum = "";
+        int q, w, sum;
 
-        if (sumStr.length() > lengthNewMas) {
+        for (int i = 0; i <= num.digits.length - 1; i++){
+            out1= out1 + Byte.toString((byte) (num.digits[i]));
+        }
+        for (int j = 0; j < digits.length; j++){
+            out2= out2 + Byte.toString((byte) (digits[j]));
+        }
+        q= Integer.parseInt(out1);
+        w= Integer.parseInt(out2);
+        sum = q+w;
+       
+        outSum = String.valueOf(sum);
+
+        if (num.digits.length != outSum.length()) {
             ok = false;
-            for (int i = 0; i < finMass.length; i++) {
-                finMass[i] = 0;
+            for (int l = 0; l < num.n; l++) {
+                num.digits[l] = 0;
+            }
+        } else {
+            String[] strArr = outSum.split("");
+            num.digits = new byte[strArr.length];
+            for (int i = 0; i < strArr.length; i++) {
+                num.digits[i] = Byte.parseByte(strArr[i]);
             }
         }
-        else {
-            fromString(sumStr);
-            }
-
         return ok;
     }
 
     public static void main(String[] args) {
         ArrayInteger w =new ArrayInteger(3);
-        w.fromString("121");
+        w.fromString("691");
+        ArrayInteger q =new ArrayInteger(2);
+        q.fromString("12");
+        w.add(q);
     }
 }
